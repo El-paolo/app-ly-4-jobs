6e7ce3faab50c1019f395880631ac6ac58780edb
El-paolo <paolocordovacontacto@gmail.com>
2023-09-10 18:46:24 -0300

    modified permissions for create users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     protected
     
     def authorize_request(kind=nil)
-        unless kind.include?(current_user.role)
+        unless kind.include?(current_user.role.to_s)
             redirect_to posts_path, notice: "no estás autorizado para realizar esta acción"
         end
     end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  skip_before_action :require_no_authentication, only: [:new, :create]
   before_action do
     authorize_request("admin")
   end
