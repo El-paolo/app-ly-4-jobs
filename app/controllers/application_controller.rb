936a359929fa05837fea9b9cadcdc9842f634178
El-paolo <paolocordovacontacto@gmail.com>
2023-09-10 16:01:00 -0300

    add the routes and the authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    protected
+    
+    def authorize_request(kind=nil)
+        unless kind.include?(current_user.role)
+            redirect_to posts_path, notice: "no estás autorizado para realizar esta acción"
+        end
+    end
 end
