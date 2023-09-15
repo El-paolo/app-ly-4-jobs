44540bbf3e7654560a54c21aceb0352ae56c4b83
El-paolo <paolocordovacontacto@gmail.com>
2023-09-14 22:05:00 -0300

    added the fields of name and phone, and the respectives configs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_09_012239) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_15_003959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_09_012239) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.integer "phone", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
